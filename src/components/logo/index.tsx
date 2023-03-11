b34be88db1c013881fded8c68bcef8b027d6ff6c
AdrianPorto <gustosts@gmail.com>
2023-03-11 04:49:12 -0300

    new

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,13 +1,35 @@
 import React from "react";
-
+import { Ubuntu } from "@next/font/google";
+import "animate.css";
 // import { Container } from './styles';
-
+const rubik = Ubuntu({
+  subsets: ["latin"],
+  weight: "400",
+});
 const Logo: React.FC = () => {
   return (
-    <div className="text-center flex flex-col justify-center text-6xl  ml-20 top-8 text-white">
-      <div>Tic</div>
-      <div>Tac</div>
-      <div>Toe</div>
+    <div
+      className={` flex flex-col justify-center text-6xl items-center content-center absolute top-2 left-96 right-96 text-white font-extrabold  ${rubik.className} `}
+      style={{
+        WebkitTextStroke: " 1px  #000",
+        animation: "running",
+      }}
+    >
+      <div className="flex flex-row   text-red-700 animate__animated animate__pulse animate__delay-1s animate__infinite	">
+        <div className="-rotate-12 mt-2">T</div>
+        <div className="-rotate-0 ml-2 mr-2">i</div>
+        <div className="rotate-12 mt-2">c</div>
+      </div>
+      <div className="flex flex-row  text-green-600 animate__animated animate__pulse animate__delay-1s  animate__infinite	">
+        <div className="-rotate-12 mt-2">T</div>
+        <div className="-rotate-0 ml-2 mr-2">a</div>
+        <div className="rotate-12 mt-2">c</div>
+      </div>
+      <div className="flex flex-row  text-red-700 animate__animated animate__pulse animate__delay-1s animate__infinite	">
+        <div className="-rotate-12 mt-2">T</div>
+        <div className="-rotate-0 ml-2 mr-2">o</div>
+        <div className="rotate-12 mt-2">e</div>
+      </div>
     </div>
   );
 };
